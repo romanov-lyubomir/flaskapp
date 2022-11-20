87854a69f4f9b202dd4dabf6a4b4a7e1400714c1
romanov-lyubomir <romanov.lyubomir+github@protonmail.com>
2022-11-20 12:05:51 +0200

    Add Procfile and requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, abort, flash, redirect, render_template, request, session, url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_compress import Compress
+from flask_login import LoginManager, UserMixin, current_user, login_required, login_user, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_socketio import SocketIO
 
+
+app = Flask(__name__)
+socket = SocketIO(app)
+Compress(app)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///users.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    language = db.Column(db.String(10), nullable=False)
+
+
+db.create_all()
 
 @app.route('/')
 def index():
